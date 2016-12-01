5b0061f58b7e188d1001bb8253c059b6f2ac12eb
Ted McElwee <ted.mcelwee@fandor.com>
2016-12-01 15:21:20 -0800

    small refactor

--- lib/acts_as_taggable_on/db_connection.rb
+++ lib/acts_as_taggable_on/db_connection.rb
@@ -7,10 +7,10 @@ module ActsAsTaggableOn
 
       def establish_tagging_database_connection
         unless Rails.env == 'test'
-          if File.exist? "#{Rails.root}/config/acts_as_taggable_on_database.yml"
-            ## Matinee DB Connection
-            class_db_connection = YAML.load_file("#{Rails.root.to_s}/config/acts_as_taggable_on_database.yml")[Rails.env]
-            #Rails.logger.debug "** Establishing connection to acts_as_taggable_on_database"
+          db_config = File.join(Rails.root, "config", "acts_as_taggable_on_database.yml")
+          if File.exist?(db_config)
+            ## alternate DB Connection
+            class_db_connection = YAML.load_file(db_config)[Rails.env]
             establish_connection class_db_connection
           end
         end
